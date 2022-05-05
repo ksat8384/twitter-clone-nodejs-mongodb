ae815b21a5756b68f4ad8322f2f16b16e6436d01
ksat8384 <ksat8384@gmail.com>
2022-05-05 10:25:52 +0530

    Reduced password length to 50 to avoid conflict with bcrypt package restriction

--- models/User.js
+++ models/User.js
@@ -48,8 +48,8 @@ User.prototype.validate = function () {
   if (this.data.password.length > 0 && this.data.password.length < 12) {
     this.errors.push("Password must be atleast 12 characters.");
   }
-  if (this.data.password.length > 100) {
-    this.errors.push("Password cannot exceed 100 characters.");
+  if (this.data.password.length > 50) {
+    this.errors.push("Password cannot exceed 50 characters.");
   }
   if (this.data.username.length > 0 && this.data.username.length < 3) {
     this.errors.push("Username must be atleast 3 characters.");
