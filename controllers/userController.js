c02a7f0e591f8f441f8d98c7540534fdf0c763e4
ksat8384 <ksat8384@gmail.com>
2022-05-27 14:52:12 +0530

    Using reusable function named mustBeLoggedIn, restricting the guest user visiting create post page.

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,17 @@
 const { urlencoded } = require("express");
 const User = require("../models/User");
 
+exports.mustBeLoggedIn = function (req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    req.flash("errors", "You must be logged in to perform that action.");
+    req.session.save(function () {
+      res.redirect("/");
+    });
+  }
+};
+
 exports.login = function (req, res) {
   let user = new User(req.body);
   //Modern approach is promise based instead of traditional callback
