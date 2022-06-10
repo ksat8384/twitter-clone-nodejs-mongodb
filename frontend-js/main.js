01550177f96ab061214ac73872504df4b3d7ea0f
ksat8384 <ksat8384@gmail.com>
2022-06-10 15:51:49 +0530

    Beginning chat feature

--- frontend-js/main.js
+++ frontend-js/main.js
@@ -1,4 +1,10 @@
 import Search from "./modules/search";
+import Chat from "./modules/chat";
+
+//To show chat only if we are logged in
+if (document.querySelector("#chat-wrapper")) {
+  new Chat();
+}
 
 //To show search only if we are logged in
 if (document.querySelector(".header-search-icon")) {
