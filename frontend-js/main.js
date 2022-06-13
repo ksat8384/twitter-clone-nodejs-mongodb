ed3686be8f47bf94909d0e95afc75aee983c2c37
ksat8384 <ksat8384@gmail.com>
2022-06-13 18:46:18 +0530

    Adding live form validation for registration fields in the frontend

--- frontend-js/main.js
+++ frontend-js/main.js
@@ -1,5 +1,10 @@
 import Search from "./modules/search";
 import Chat from "./modules/chat";
+import RegistrationForm from "./modules/registrationForm";
+
+if (document.querySelector("#registration-form")) {
+  new RegistrationForm();
+}
 
 //To show chat only if we are logged in
 if (document.querySelector("#chat-wrapper")) {
---
+++ frontend-js/modules/registrationForm.js
@@ -0,0 +1,40 @@
+export default class RegistrationForm {
+  constructor() {
+    this.allFields = document.querySelectorAll(
+      "#registration-form .form-control"
+    );
+    this.insertValidationElements();
+    this.username = document.querySelector("#username-register");
+    this.username.previousValue = "";
+    this.events();
+  }
+
+  //Events
+  events() {
+    this.username.addEventListener("keyup", () => {
+      this.isDifferent(this.username, this.usernameHandler);
+    });
+  }
+
+  //Methods
+  isDifferent(el, handler) {
+    if (el.previousValue != el.value) {
+      //To avoid changing "this" pointing to global object if we call "handler()" here...
+      handler.call(this);
+    }
+    el.previousValue = value;
+  }
+
+  usernameHandler() {
+    alert("Username handler just ran");
+  }
+
+  insertValidationElements() {
+    this.allFields.forEach(function (el) {
+      el.insertAdjacentHTML(
+        "afterend",
+        '<div class="alert alert-danger small liveValidateMessage"></div>'
+      );
+    });
+  }
+}
